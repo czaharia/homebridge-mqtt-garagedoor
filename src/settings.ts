1f0cd8ef14892a25b27a0fb2d8af5e5f69e37192
Cristi <cristi@brigada.ro>
2026-04-02 01:06:09 +0300

    cosmetic changes

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
 export const PLATFORM_NAME = 'HCPBridgeMqttHomebridgePlugin';
-export const PLUGIN_NAME = 'homebridge-garagedoor-hcpbridgemqtt';
+export const PLUGIN_NAME = 'homebridge-hcpbridgemqtt';
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const PLUGIN_VERSION = '0.2.4';
+export const PLUGIN_VERSION = '0.2.5';
